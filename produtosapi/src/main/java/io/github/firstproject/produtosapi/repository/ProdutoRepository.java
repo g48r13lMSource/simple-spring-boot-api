8e2fb54c2c28ee7a4a5f28571acc8e647c398597
g48r13lMSource <gabriel.maracaja@labchecap.com.br>
2025-09-30 21:39:30 -0300

    First Commit

---
+++ produtosapi/src/main/java/io/github/firstproject/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package io.github.firstproject.produtosapi.repository;
+
+import io.github.firstproject.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
